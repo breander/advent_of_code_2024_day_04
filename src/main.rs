8db6ce874873899ae9cbe48b207efe25eec1667d
Brian Davenport <breander@gmail.com>
2024-12-05 11:08:07 -0600

    Part 1 Complete

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,106 @@ fn main() {
     let buffer = fs::read_to_string(file_path).unwrap();
     let lines = buffer.lines();
 
+    // setup grid and counts
+    let mut grid: Vec<Vec<char>> = Vec::new();
+    let mut line_count = 0;
+    let mut col_count = 0;
+    let mut part1_count = 0;
+
+    // load text into 2d vector
     for line in lines {
-        for (i, c) in line.chars().enumerate() {
-            println!("{}, {}", i, c);
+        let characters: Vec<char> = line.chars().collect(); 
+        col_count = characters.len();
+        grid.push(characters);
+        line_count += 1;
+    }
+
+    let mut right = 0;
+    let mut right_down = 0;
+    let mut down = 0;
+    let mut down_left = 0;
+    let mut left = 0;
+    let mut left_up = 0;
+    let mut up = 0;
+    let mut up_right = 0;
+
+    // iterate over grid to check for XMAS
+    for row in 0..line_count {
+        for col in 0..col_count {
+            // check right
+            if (col + 4) <= col_count {
+                if grid[row][col] == 'X' && grid[row][col+1] == 'M' && grid[row][col+2] == 'A' && grid[row][col+3] == 'S' {
+                    part1_count += 1;
+                    right += 1;
+                }
+            }
+
+            // check right + down
+            if (col + 4) <= col_count && (row + 4) <= line_count {
+                if grid[row][col] == 'X' && grid[row+1][col+1] == 'M' && grid[row+2][col+2] == 'A' && grid[row+3][col+3] == 'S' {
+                    part1_count += 1;
+                    right_down += 1;
+                }
+            }
+
+            // check down
+            if (row + 4) <= line_count {
+                if grid[row][col] == 'X' && grid[row+1][col] == 'M' && grid[row+2][col] == 'A' && grid[row+3][col] == 'S' {
+                    part1_count += 1;
+                    down += 1;
+                }
+            }
+
+            // check down + left
+            if (row + 4) <= line_count && (col as i32 - 3) >= 0 {
+                if grid[row][col] == 'X' && grid[row+1][col-1] == 'M' && grid[row+2][col-2] == 'A' && grid[row+3][col-3] == 'S' {
+                    part1_count += 1;
+                    down_left += 1;
+                }
+            }
+
+            // check left
+            if (col as i32 - 3) >= 0 {
+                if grid[row][col] == 'X' && grid[row][col-1] == 'M' && grid[row][col-2] == 'A' && grid[row][col-3] == 'S' {
+                    part1_count += 1;
+                    left += 1;
+                }
+            }
+
+            // check left + up
+            if (col as i32 - 3) >= 0 && (row as i32 - 3) >= 0 {
+                if grid[row][col] == 'X' && grid[row-1][col-1] == 'M' && grid[row-2][col-2] == 'A' && grid[row-3][col-3] == 'S' {
+                    part1_count += 1;
+                    left_up += 1;
+                }
+            }
+
+            // check up
+            if (row as i32 - 3) >= 0 {
+                if grid[row][col] == 'X' && grid[row-1][col] == 'M' && grid[row-2][col] == 'A' && grid[row-3][col] == 'S' {
+                    part1_count += 1;
+                    up += 1;
+                }
+            }
+
+            // check up + right
+            if (row as i32 - 3) >= 0 && (col + 4) <= col_count {
+                if grid[row][col] == 'X' && grid[row-1][col+1] == 'M' && grid[row-2][col+2] == 'A' && grid[row-3][col+3] == 'S' {
+                    part1_count += 1;
+                    up_right += 1;
+                }
+            }
         }
     }
+
+    println!("right: {right}");
+    println!("right_down: {right_down}");
+    println!("down: {down}");
+    println!("down_left: {down_left}");
+    println!("left: {left}");
+    println!("left_up: {left_up}");
+    println!("up: {up}");
+    println!("up_right: {up_right}");
+    println!("part 1 XMAS count: {part1_count}");
 }
 
